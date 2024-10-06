e338882a96a8c5dfa7376a6eb4110cf58458c830
Saksham KC <sakshamkc@Sakshams-MacBook-Air.local>
2024-10-06 12:33:50 +0545

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav className="poppins-extralight">
+        <div className="logo ">
+          <img src="../../images/brand_logo.png" alt="nike logo" />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </>
+  );
 }
 
 export default App;
